b1f75973bd41a286877c2d556444f1b966c9d681
Tom-bot-not-a-person <tomnacestach.dotaz@gmail.com>
2025-06-27 16:57:55 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,8 @@
 const audio = document.getElementById('audioPlayer');
-const playBtn1 = document.getElementById('playBtn1');
-const playBtn2 = document.getElementById('playBtn2');
+const playBtn = document.getElementById('playBtn');
 const stopBtn = document.getElementById('stopBtn');
 
-// Cesty k písničkám
-const song1 = 'choose_your_fighter.mp3';
-const song2 = 'druha_pisen.mp3';  // sem dej název druhé písničky
-
-playBtn1.addEventListener('click', () => {
-  if (audio.src !== location.origin + '/' + song1) {
-    audio.src = song1;
-  }
-  audio.play();
-});
-
-playBtn2.addEventListener('click', () => {
-  if (audio.src !== location.origin + '/' + song2) {
-    audio.src = song2;
-  }
+playBtn.addEventListener('click', () => {
   audio.play();
 });
 
